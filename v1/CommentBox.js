2053f616d565269bdebe482fbd0ef7f4cd04b58e
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2022-10-11 10:02:16 +0200

    Ajout des fichiers version 1

---
+++ v1/CommentBox.js
@@ -0,0 +1,57 @@
+// CommentBox.js
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+import CommentList from './CommentList';
+//import CommentForm from './CommentForm';
+import './CommentBox.css';
+
+class CommentBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      comment: '',
+      updateId: null,
+    };
+    this.pollInterval = null;
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    // fetch returns a promise. If you are not familiar with promises, see
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="comments">
+          <h2>Comments:</h2>
+          <CommentList data={this.state.data} />
+        </div>
+        {this.state.error && <p>{this.state.error}</p>}
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
---
+++ v1/CommentList.js
@@ -0,0 +1,32 @@
+// CommentList.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+
+const CommentList = (props) => {
+  const commentNodes = props.data.map(comment => (
+    <Comment author={comment.author} key={comment._id} id={comment._id}>
+      { comment.text}
+    </Comment>
+  ));
+
+  return (
+    <div>
+      { commentNodes }
+    </div>
+  );
+};
+
+CommentList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    author: PropTypes.string,
+    id: PropTypes.string,
+    text: PropTypes.string,
+  })),
+};
+
+CommentList.defaultProps = {
+  data: [],
+};
+
+export default CommentList;
